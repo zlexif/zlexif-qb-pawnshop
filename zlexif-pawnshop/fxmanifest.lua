5681b080fea2d3b1cfd2cd3fc307c8a5306ad35d
zlexif <146517988+zlexif@users.noreply.github.com>
2023-10-29 15:38:44 +0200

    Update fxmanifest.lua

--- zlexif-pawnshop/fxmanifest.lua
+++ zlexif-pawnshop/fxmanifest.lua
@@ -1,6 +1,6 @@
 fx_version 'cerulean'
-author '# Ahmad / ranjit'
-Description 'A vanilla unicorn job script edited by ahmad.'
+author '# Ahmad'
+Description 'A pawn shop job script by ahmad.'
 game 'gta5'
 
 shared_scripts { 
