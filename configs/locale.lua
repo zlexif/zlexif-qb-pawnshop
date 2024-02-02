234875d7b8fca5df94db005b573c9c20bfed8b9d
zlexif <146517988+zlexif@users.noreply.github.com>
2024-02-03 00:54:37 +0200

    HUGE UPDATE
    - Now supports : OkOkNotify , ps-notify/ui , k5-notify , qbcore notifications ( AND MORE IN THE FUTURE DO LET ME KNOW WHAT U GUYS USE )
    - Also supports qs-inventory and qb-inventory , supports ox target and qb target. and any more targets that use the same exports and formats.
    - New Locker system with stashes.
    - Updated the old stash system too , now it has unique identifiers allowing you to create as many as u want with configurable weight and slots and name.
    - Interactions : Added an option to sleep on 2 beds where you relieve stress when done so , for rp immersiveness.
    - Added an illegal money washing business to it downstairs too.
    - Added new breaking machine , where you can break configurable items for configurable mats and amounts.
    - Rewrote all code from scratch.
    -  Optimised it significantly and removed unnecesarry lines etc.

---
+++ configs/locale.lua
@@ -0,0 +1,44 @@
+Language = {}
+
+Language = {
+    Input = {
+        Header = "Billing",
+        Submit = "Submit",
+        Paypal = "Paypal ID",
+        Amount = "Amount",
+    },
+    Progressbars = {
+        Make = "Making... ",
+        Ticket = "Taking a ticket... "
+    },
+    Menu = {
+        Craft = "[🔨] Crafting",
+        Close = "[❌] Close",
+        Required = "Required:",
+    },
+    Notify = {
+    Made = "You made: ",
+    Cancelled = "Cancelled",
+    NoMaterials = "You don't have the necessary materials dipshit.",
+    Send = "Invoice sent successfully.",
+    HigherValue = "Must be a valid value above 0. Do you not know basic maths?",
+    InvoiceOwn = "You can't bill yourself, Stupid!",
+    PlayerUnavailable = "Player might be in heaven",
+    NoPermission = "Derp, you dont have the needed permission!",
+    InvoiceReceived = "New invoice received!",
+    RunStarted = "Run has started, you have 3 minutes to complete!",
+    RunFailedTime = "You failed to complete the run on time!",
+    NoRequiredJob = "You do not have the required job",
+    AlreadyOnRun = "You are already on a run!",
+    PedAlreadySpawned = "A destination ped is already spawned",
+    ItemsReceived = "You have received your items",
+    NoActiveRun = "No active run or run has already ended.",
+    RunEnded = "Run has been ended.",
+    NoActiveRunToEnd = "No active run to end.",
+    InvalidAmountEntered = "Invalid amount entered",  
+    MoneyLaunderingComplete = "Money laundering complete.",  
+    NotEnoughMarkedBills = "You don't have enough marked bills to wash",  
+    RestAndStressFree = "You feel rested and stress-free.",  
+    InvalidLockerNumber = "Invalid locker number. Please enter a number between 1 and 999."  
+}
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,28 @@
+fx_version 'cerulean'
+author '# Ahmad'
+Description 'A pawn shop job script by ahmad.'
+game 'gta5'
+
+shared_scripts { 
+    "configs/**.lua",
+}
+
+server_script { 
+    "@oxmysql/lib/MySQL.lua",
+    "server/**.lua",
+}
+
+client_script {
+    '@PolyZone/client.lua',
+    '@PolyZone/BoxZone.lua',
+    '@PolyZone/EntityZone.lua',
+    '@PolyZone/CircleZone.lua',
+    '@PolyZone/ComboZone.lua',
+    "client/**.lua",
+}
+
+dependencies {
+    'qb-core',
+    'PolyZone',
+    'qb-menu'
+}
